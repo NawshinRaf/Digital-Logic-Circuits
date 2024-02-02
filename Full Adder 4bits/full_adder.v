8d915e9bcdabbbc7b6686d3afe14f7d6f323d402
NawshinRaf <43382522+NawshinRaf@users.noreply.github.com>
2024-02-02 12:28:27 -0800

    Update full_adder.v

--- Full Adder 4bits/full_adder.v
+++ Full Adder 4bits/full_adder.v
@@ -1,1 +1,13 @@
-
+module full_adder(
+    output Sum,
+    output Carryout,
+    input input_X,
+    input input_Y,
+    input input_Z
+    );
+    
+    assign Sum = input_X^input_Y^input_Z;
+    assign Carryout = (input_X&input_Y) | (input_Z&(input_X^input_Y));
+    
+    
+endmodule
